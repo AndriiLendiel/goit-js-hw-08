fbb22d3b0cf0eac4001ee9fee090ce18dd286a9a
AndriiLendiel <lendiel9333@gmail.com>
2023-10-29 17:30:21 +0200

    test

---
+++ src/js/01-gallery.js
@@ -0,0 +1,27 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import  'simplelightbox/dist/simple-lightbox.min.css';
+
+
+
+
+
+
+
+const gallaryList = document.querySelector('.gallery');
+
+
+function makeGallarylist() {
+return galleryItems.map(
+    ({preview, original, description}) => 
+`<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+).join('');
+
+}
+gallaryList.innerHTML = makeGallarylist();
+
+  let lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: "alt"})
+
